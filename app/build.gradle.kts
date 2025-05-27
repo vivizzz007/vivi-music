223fa5661d6c380539f5366e35b5709d832559be
vivi <mkmdevilmi@gmail.com>
2025-05-27 07:30:07 +0530

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "2.0.3"
+        versionName = "2.0.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
