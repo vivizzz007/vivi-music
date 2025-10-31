bb01f16d8624663c73ab05ceaf10c5524b03995e
vivizzz007 <mkmdevilmi@gmail.com>
2025-10-31 16:00:01 +0530

    Updated to v4.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.vivi.vivimusic"
         minSdk = 26
         targetSdk = 36
-        versionCode = 53
-        versionName = "4.0.4"
+        versionCode = 13 //13 //53
+        versionName = "4.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
