b65128ef708b88e6c57f91893230199f1bde0ec0
vivizzz007 <mkmdevilmi@gmail.com>
2026-03-04 10:32:45 +0530

    fixed version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 63
-        versionName = "5.0.2"
+        versionName = "5.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
