f929ec951f9c9abf760fa36ffb1f7665bf09a059
vivizzz007 <mkmdevilmi@gmail.com>
2026-04-06 18:30:25 +0530

    Updated string and color

--- app/src/main/kotlin/com/music/vivi/ui/screens/settings/AboutScreen.kt
+++ app/src/main/kotlin/com/music/vivi/ui/screens/settings/AboutScreen.kt
@@ -239,7 +239,7 @@ fun AboutScreen(
                 Material3SettingsItem(
                     icon = painterResource(R.drawable.dev),
                     title = { Text(stringResource(R.string.developer_name)) },
-                    description = { Text(stringResource(R.string.app_developer)) },
+                    description = { Text(stringResource(R.string.app_developer), color = MaterialTheme.colorScheme.primary) },
                     tintIcon = false,
                     iconShape = cookieShape,
                     onClick = { uriHandler.safeOpenUri(context, "https://github.com/vivizzz007") }
