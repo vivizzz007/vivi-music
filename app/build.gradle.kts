ad8d5d403677a37af45d8d1cabbb38121cbe3580
vivizzz007 <mkmdevilmi@gmail.com>
2026-07-07 21:08:58 +0530

    Bump version to 6.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.vivi.vivimusic"
         minSdk = 26
         targetSdk = 36
-        versionCode = 73
-        versionName = "6.0.4"
+        versionCode = 74
+        versionName = "6.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
