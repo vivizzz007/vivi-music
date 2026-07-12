f47df2cb7d5fb2d8398823f8ba9c9c0cf3be9473
vivizzz007 <mkmdevilmi@gmail.com>
2026-07-12 11:17:40 +0530

    - Added ANDROID_MUSIC and IOS_MUSIC client profiles to YouTubeClient

--- innertube/src/main/kotlin/com/music/innertube/models/YouTubeClient.kt
+++ innertube/src/main/kotlin/com/music/innertube/models/YouTubeClient.kt
@@ -242,5 +242,25 @@ data class YouTubeClient(
             useSignatureTimestamp = false,
             packageName = "com.google.ios.youtube"
         )
+
+        val ANDROID_MUSIC = YouTubeClient(
+            clientName = "ANDROID_MUSIC",
+            clientVersion = "6.37.50",
+            clientId = "73",
+            userAgent = "com.google.android.apps.youtube.music/6.37.50 (Linux; U; Android 14) gzip",
+            friendlyName = "Android Music",
+            loginSupported = true,
+            useSignatureTimestamp = true
+        )
+
+        val IOS_MUSIC = YouTubeClient(
+            clientName = "IOS_MUSIC",
+            clientVersion = "6.37.1",
+            clientId = "74",
+            userAgent = "com.google.ios.youtubemusic/6.37.1 (iPhone16,2; U; CPU iOS 18_2 like Mac OS X;)",
+            friendlyName = "iOS Music",
+            loginSupported = true,
+            useSignatureTimestamp = true
+        )
     }
 }
