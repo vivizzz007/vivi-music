863983a734234a4b196859cba78315f4917cb917
vivizzz007 <mkmdevilmi@gmail.com>
2026-09-25 09:51:25 +0530

    chore(release): update APK and changelog for v6.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,9 +25,9 @@ android {
         applicationId = "com.vivi.vivimusic"
         minSdk = 26
         targetSdk = 37
-        versionCode = 76
+        versionCode = 77
         val betaVersionName = project.findProperty("betaVersionName") as String?
-        versionName = betaVersionName ?: "6.0.7"
+        versionName = betaVersionName ?: "6.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
